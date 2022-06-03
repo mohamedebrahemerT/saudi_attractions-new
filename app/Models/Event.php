e370bf1176c5f0c9c246f106f0580887de6c482c
mohamedebrahemerT <mohamedchi2013@gmail.com>
2022-06-03 14:16:10 +0200

    asAS
    ASAs

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-//use Dimsav\Translatable\Translatable;
+use Astrotomic\Translatable\Translatable;
 use Eloquent as Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -23,7 +23,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Event extends Model
 {
     use SoftDeletes;
-   // use Translatable;
+    use Translatable;
 
     public $table = 'events';
     
