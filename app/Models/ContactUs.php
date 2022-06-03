cbc3d4f04969916cee4ea40cb4317143aa014900
mohamedebrahemerT <mohamedchi2013@gmail.com>
2022-06-03 14:24:34 +0200

    sadasd
    asdasd

--- app/Models/ContactUs.php
+++ app/Models/ContactUs.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Eloquent as Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Dimsav\Translatable\Translatable;
+use Astrotomic\Translatable\Translatable;
 
 
 /**
